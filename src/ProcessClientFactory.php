ad2be2b732a17ae9dbdfbb5fe433c320a582843e
Juraj Papp <jurahu@jurahu.cz>
2017-10-12 12:41:56 +0200

    Doc comment for factory

--- src/ProcessClientFactory.php
+++ src/ProcessClientFactory.php
@@ -6,6 +6,15 @@ use GuzzleHttp\Client;
 
 class ProcessClientFactory
 {
+	/**
+	 * Creates process client
+	 *
+	 * @param string $url
+	 * @param string $user
+	 * @param string $password
+	 *
+	 * @return ProcessClient
+	 */
 	public static function create($url, $user, $password)
 	{
 		$guzzle = new Client();
